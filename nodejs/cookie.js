3899bc20851bedae4a1901ec15e0e70ad7c531e8
KrillM <mks160615@gmail.com>
2023-10-20 12:25:48 +0900

    기본쿠키 추가

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -5,13 +5,22 @@ http.createServer(function(req, res){
     console.log(req.headers.cookie);
     let cookies = {};
     
-    if(req.headers.cookie!=undefined){
+    if(req.headers.cookie!==undefined){
         cookies=cookie.parse(req.headers.cookie);
+        // parse 함수는 쿠키가 사라지면 그냥 에러 메시지 전송한다.
     }
 
     console.log(cookies.yummy_cookie);
     res.writeHead(200, {
-        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie':[
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=Path; Path=/cookie',
+            'Doamin=Domain; Domain=test.o2.org'
+        ]
     });
     res.end("Cookie Monster!!");
 }).listen(3000);
